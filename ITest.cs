f378d85db87c2b089af6a0fa602e276eb2194ad6
ShadidIntisar99 <60622370+ShadidIntisar99@users.noreply.github.com>
2020-12-31 23:57:54 +0600

    Create ITest.cs

---
+++ ITest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class Test1
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver("C:\\Users\\Shadid Intisar\\Desktop\\Selenium\\chromedriver.exe");
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser 
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void The1Test()
+        {
+            driver.Navigate().GoToUrl("https://elp.duetbd.org/");
+            driver.FindElement(By.LinkText("Log in")).Click();
+            driver.FindElement(By.Id("username")).Click();
+            driver.FindElement(By.Id("password")).Click();
+            driver.FindElement(By.XPath("//form[@id='login']/div[3]")).Click();
+            driver.FindElement(By.Id("rememberusername")).Click();
+            driver.FindElement(By.Id("loginbtn")).Click();
+            driver.FindElement(By.XPath("//div[@id='page-container-2']/div/div/div/a/div")).Click();
+            driver.FindElement(By.XPath("//img[contains(@src,'https://elp.duetbd.org/theme/image.php/academi/core/1606326980/u/f2_white')]")).Click();
+            driver.FindElement(By.Id("actionmenuaction-6")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
